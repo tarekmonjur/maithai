60c68072619454fd2179a6ca741103fbbfde2171
asadanik <s.engineer63@gmail.com>
2021-02-23 20:18:31 +0600

    Changed designs of Home Gallery and Food Menu

--- theme/js/collapse_button.js
+++ theme/js/collapse_button.js
@@ -142,6 +142,26 @@
 
 
 
+
+
+
+    // POPULAR DISHES Section Of Home Page...
+    $('.controls .buttons').click(function(){
+
+        $(this).addClass('button-active').siblings().removeClass('button-active');
+
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.dish .image').show(400);
+        }else{
+            $('.dish .image').not('.'+filter).hide(200);
+            $('.dish .image').filter('.'+filter).show(400);
+        }
+
+    });
+
+
+
 })(jQuery);
 
 
